536804ec3af796a437953223ec5e72c2ddf361f6
Erick Silva <donizttierick@gmail.com>
2025-12-07 18:00:08 -0300

    Ajustando

--- script.js
+++ script.js
@@ -5,19 +5,27 @@ let s = document.getElementById('segundos')
 let mile = document.getElementById('milessegundo')
 
 indice = 0
+index = 1
 // Função
 function iniciar() {
     setInterval(function(){
         indice++
+        
         mile.innerText = String(mile.innerText = (indice)).padStart(2, '0')
 
-        if(indice == 59) {
+        if (indice == 10 ) {
             indice = 0
+            s.innerText = index++
+            return
+        }else if (index == 59) {
+            index = 0
             return
         }
 
         
 
         
-    }, 10)
+
+        
+    }, 50)
 }
