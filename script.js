bbf929e0809021dcf39408c49c89a2a96e165bc8
Erick Silva <donizttierick@gmail.com>
2025-12-07 17:49:17 -0300

    Finalizado

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 let h = document.getElementById('horas')
 let m = document.getElementById('minutos')
 let s = document.getElementById('segundos')
+let mile = document.getElementById('milessegundo')
 
+indice = 0
 // Função
 function iniciar() {
-    
+    setInterval(function(){
+        indice++
+        mile.innerText = String(mile.innerText = (indice)).padStart(2, '0')
+
+        if(indice == 59) {
+            indice = 0
+            return
+        }
+
+        
+
+        
+    }, 10)
 }
