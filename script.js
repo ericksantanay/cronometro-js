327b32a1c8dd39111b22d185fcfd949ee80a632c
Erick Silva <donizttierick@gmail.com>
2025-12-07 17:29:44 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+// Saidas
+let h = document.getElementById('horas')
+let m = document.getElementById('minutos')
+let s = document.getElementById('segundos')
+
+// Função
+function iniciar() {
+    
+}
